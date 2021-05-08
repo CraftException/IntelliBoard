5f56a612ada5afa382fec7f551ce2de9279cffef
CraftException <55949980+LiamFiedler@users.noreply.github.com>
2021-05-08 16:27:01 +0200

    Added Shell Scripts

---
+++ install.sh
@@ -0,0 +1,50 @@
+# IntelliBoard - Copyright (C) 2021 Moritz Kaufmann
+# Install Script for IntelliBoard
+echo "IntelliBoard Installer v0.0.1"
+echo "Install prerequisites..."
+
+# Install git to download IntelliBoard
+sudo apt install git
+
+# Install Screen
+sudo apt install screen
+
+# Install Node and npm for prerequisites
+sudo apt install nodejs npm
+
+# Download IntelliBoard
+clear
+echo "Download IntelliBoard..."
+
+git clone https://github.com/CraftException/IntelliBoard intelliboard
+cd intelliboard
+clear
+
+# Set Mongo Input
+echo "Input your MongoDB URI: "
+read Uri
+
+cat /dev/null > Backend/mongouri.txt
+
+cat >> Backend/mongouri.txt <<EOF
+$Uri
+EOF
+
+# Port Info
+export PORT=3000
+echo "Set the default port to 3000. To update the port, type 'export PORT=<port>'"
+
+# Install and Compile typescript
+echo "Build Files..."
+npm install -g typescript
+npm run compile-ts
+
+# Make shell scripts executable
+chmod +x start.sh
+chmod +x stop.sh 
+
+# Start IntelliBoard
+echo "IntelliBoard is now installed. It'll start now."
+echo "To stop IntelliBoard, type './stop.sh'"
+echo "To start IntelliBoard again, type './start.sh'"
+./start.sh
---
+++ start.sh
@@ -0,0 +1,1 @@
+screen -dm -S intelliboard node bin/www
